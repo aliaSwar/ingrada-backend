83630ed0c6933f0b0613cf15a2d70ea064ad2595
ayaasfoor <aya.asfour.it@gmail.com>
2023-04-09 13:22:51 +0300

    Adding Item controller

---
+++ app/Http/Requests/StoreItemRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreItemRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+
+            'the design'         =>      ['required','string', 'min:10', 'max:100'],
+            'scope_id'           =>      ['required','numeric', 'exists:scopes,id'],
+            'type_id'            =>      ['required','numeric', 'exists:types,id'],
+            'prefernce_id'       =>      ['required','numeric', 'exists:prefernces,id'],
+            'prefernce_value_id' =>      ['nullable','numeric', 'exists:prefernce_values,id'],
+            'is_enable_post'     =>      ['required','numeric'],
+            'likes'              =>      ['required','numeric'],
+            'price'              =>      ['required','numeric'],
+
+        ];
+    }
+
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            sendErrorResponse($validator->errors()->first(), null, 422)
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ColorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('colors', ColorController::class);
