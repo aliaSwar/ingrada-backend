e2b487da31d45efb1ef1df48864772c41913da6f
ayaasfoor <aya.asfour.it@gmail.com>
2023-04-13 13:07:31 +0300

    wip

---
+++ app/Http/Requests/StoreColorItemRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreColorItemRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'color_id'           =>      ['required','numeric', 'exists:colors,id'],
+            'item_id'            =>      ['required','numeric', 'exists:items,id'],
+        ];
+    }
+
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            sendErrorResponse($validator->errors()->first(), null, 422)
+        );
+    }
+}
