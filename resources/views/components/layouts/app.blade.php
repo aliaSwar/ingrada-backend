fb7118672ba5b18e170733441078a2fefc26e777
swar144 <130346497+aliasewar@users.noreply.github.com>
2023-04-15 20:47:33 +0300

    app

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,3 +1,107 @@
-<div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
-</div>
+ <!DOCTYPE html>
+ <html dir="ltr" lang="en">
+
+ <head>
+     <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <!-- Tell the browser to be responsive to screen width -->
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="description" content="">
+     <meta name="author" content="">
+     <!-- Favicon icon -->
+     <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+     <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
+     <title>Freedash Template - The Ultimate Multipurpose admin template</title>
+
+     <link rel="stylesheet" href="../assets/libs/chart.js/dist/apex-charts.css" />
+     <!-- Custom CSS -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/charts.css/dist/charts.min.css">
+     <link href="../assets/extra-libs/c3/c3.min.css" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+     <link href="../assets/libs/chartist/dist/chartist.min.css" rel="stylesheet">
+     <link href="../assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
+     <!-- Custom CSS -->
+     <link href="../dist/css/style.min.css" rel="stylesheet">
+     <link type="text/css" href="@@path/vendor/apexcharts/dist/apexcharts.css" rel="stylesheet">
+     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+     <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+     <!-- ============================================================== -->
+     <!-- End Wrapper -->
+     <!-- ============================================================== -->
+     <!-- End Wrapper -->
+     <!-- ============================================================== -->
+     <!-- All Jquery -->
+     <!-- ============================================================== -->
+     <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+     <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+     <!-- apps -->
+     <!-- apps -->
+     <script src="../dist/js/app-style-switcher.js"></script>
+     <script src="../dist/js/feather.min.js"></script>
+     <script src="../assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+     <script src="../dist/js/sidebarmenu.js"></script>
+     <!--Custom JavaScript -->
+     <script src="../dist/js/custom.min.js"></script>
+     <!--This page JavaScript -->
+     <script src="../assets/extra-libs/c3/d3.min.js"></script>
+
+     <script src="../assets/extra-libs/c3/c3.min.js"></script>
+     <script src="../assets/libs/chartist/dist/chartist.min.js"></script>
+     <script src="../assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+     <script src="../assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js"></script>
+     <script src="../assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js"></script>
+     <script src="../dist/js/pages/dashboards/dashboard1.min.js"></script>
+     <!-- Vendors JS -->
+     <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
+     <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+     <!-- Main JS -->
+     <script src="../assets/js/main.js"></script>
+
+     <!-- Page JS -->
+     <script src="../assets/js/dashboards-analytics.js"></script>
+     <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+     <script src="TableFilter.min.js" defer></script>
+     <script>
+         try {
+             fetch(new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", {
+                 method: 'HEAD',
+                 mode: 'no-cors'
+             })).then(function(response) {
+                 return true;
+             }).catch(function(e) {
+                 var carbonScript = document.createElement("script");
+                 carbonScript.src = "//cdn.carbonads.com/carbon.js?serve=CE7DC2JW&placement=wwwcssscriptcom";
+                 carbonScript.id = "_carbonads_js";
+                 document.getElementById("carbon-block").appendChild(carbonScript);
+             });
+         } catch (error) {
+             console.log(error);
+         }
+
+
+         (function(i, s, o, g, r, a, m) {
+             i['GoogleAnalyticsObject'] = r;
+             i[r] = i[r] || function() {
+                 (i[r].q = i[r].q || []).push(arguments)
+             }, i[r].l = 1 * new Date();
+             a = s.createElement(o),
+                 m = s.getElementsByTagName(o)[0];
+             a.async = 1;
+             a.src = g;
+             m.parentNode.insertBefore(a, m)
+         })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+         ga('create', 'UA-46156385-1', 'cssscript.com');
+         ga('send', 'pageview');
+     </script>
+     <script src="../dist/js/TableFilter.min.js"></script>
+
+     </body>
+
+ </html>
