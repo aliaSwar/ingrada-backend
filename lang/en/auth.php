aa6469960103de14efd7c1a8cff0ad98c56a866d
swar144 <130346497+aliasewar@users.noreply.github.com>
2023-04-27 02:06:51 +0300

    Publish Language files

---
+++ lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'success_login'   => 'You have been successfully authenticated. Now you can <a href=":url">log in</a>.',
+    'failed'          => 'These credentials do not match our records.',
+    'password'        => 'The provided password is incorrect.',
+    'throttle'        => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
---
+++ lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'get_data' => 'Resource fetched successfully.',
+    'create_data' => 'Resource created successfully.',
+    'update_data' => 'Resource updated successfully.',
+    'delete_data' => 'Resource deleted successfully.',
+];
