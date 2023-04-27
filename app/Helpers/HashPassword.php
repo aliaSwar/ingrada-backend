4529b95693ac8abf0970b9eebd5fb4d1120cc7a4
Omer Baflah <aldonramos@gmail.com>
2023-04-27 18:14:04 +0300

    Update hash password helper

--- app/Helpers/HashPassword.php
+++ app/Helpers/HashPassword.php
@@ -7,11 +7,10 @@ use Illuminate\Support\Facades\Hash;
 /**
  * Returns a hash Password string.
  *
+ * @param string|null $password
  * @return string
  */
-function defaultHashedPassword(string|null $password = 'super  admin')
+function defaultHashedPassword(string|null $password = 'super  admin'): string
 {
-    $hashed_password = Hash::make($password);
-
-    return $hashed_password;
+    return Hash::make($password);
 }
