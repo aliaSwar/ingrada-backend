de86917480736ede79fe4f71ea9a8a4dd94aa3e5
alia-sewar <130346497+alia-sewar@users.noreply.github.com>
2023-05-02 02:04:30 +0300

    update link

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,30 +1,47 @@
 <!-- ============================================================== -->
 <!-- Topbar header - style you can find in pages.scss -->
 <!--==============================================================-->
-<header class="topbar" data-navbarbg="skin6">
+<header class="topbar"
+    data-navbarbg="skin6">
     <nav class="navbar top-navbar navbar-expand-lg">
-        <div class="navbar-header" data-logobg="skin6">
+        <div class="navbar-header"
+            data-logobg="skin6">
             <!-- This is for the sidebar toggle which is visible on mobile only -->
-            <a class="nav-toggler waves-effect waves-light d-block d-lg-none" href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
+            <a class="nav-toggler waves-effect waves-light d-block d-lg-none"
+                href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
             <x-logo href="/" />
             <!--==============================================================-->
             <!-- ============================================================== -->
             <!-- Toggle which is visible on mobile only -->
             <!-- ============================================================== -->
-            <a class="topbartoggler d-block d-lg-none waves-effect waves-light" href="javascript:void(0)" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i class="ti-more"></i></a>
+            <a class="topbartoggler d-block d-lg-none waves-effect waves-light"
+                href="javascript:void(0)"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"><i class="ti-more"></i></a>
         </div>
         <!-- ============================================================== -->
         <!-- End Logo -->
         <!-- ============================================================== -->
-        <div class="navbar-collapse collapse" id="navbarSupportedContent">
+        <div class="navbar-collapse collapse"
+            id="navbarSupportedContent">
             <!-- ============================================================== -->
             <!-- toggle and nav items -->
             <!-- ============================================================== -->
             <ul class="navbar-nav float-left me-auto ms-3 ps-1">
                 <!-- Notification -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle pl-md-3 position-relative" href="javascript:void(0)" id="bell" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span><i data-feather="bell" class="svg-icon"></i></span>
+                    <a class="nav-link dropdown-toggle pl-md-3 position-relative"
+                        href="javascript:void(0)"
+                        id="bell"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false">
+                        <span><i data-feather="bell"
+                                class="svg-icon"></i></span>
                         <span class="badge text-bg-warning notify-no rounded-circle">5</span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-left mailbox animated bounceInDown">
@@ -32,8 +49,10 @@
                             <li>
                                 <div class="message-center notifications position-relative">
                                     <!-- Message -->
-                                    <a href="javascript:void(0)" class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                        <div class="btn btn-danger rounded-circle btn-circle"><i data-feather="airplay" class="text-white"></i></div>
+                                    <a href="javascript:void(0)"
+                                        class="message-item d-flex align-items-center border-bottom px-3 py-2">
+                                        <div class="btn btn-danger rounded-circle btn-circle"><i data-feather="airplay"
+                                                class="text-white"></i></div>
                                         <div class="w-75 d-inline-block v-middle ps-2">
                                             <h6 class="message-title mb-0 mt-1">Luanch Admin</h6>
                                             <span class="font-12 text-nowrap d-block text-muted">Just see
@@ -43,8 +62,11 @@
                                         </div>
                                     </a>
                                     <!-- Message -->
-                                    <a href="javascript:void(0)" class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                        <span class="btn btn-success text-white rounded-circle btn-circle"><i data-feather="calendar" class="text-white"></i></span>
+                                    <a href="javascript:void(0)"
+                                        class="message-item d-flex align-items-center border-bottom px-3 py-2">
+                                        <span class="btn btn-success text-white rounded-circle btn-circle"><i
+                                                data-feather="calendar"
+                                                class="text-white"></i></span>
                                         <div class="w-75 d-inline-block v-middle ps-2">
                                             <h6 class="message-title mb-0 mt-1">Event today</h6>
                                             <span class="font-12 text-nowrap d-block text-muted text-truncate">Just
@@ -53,8 +75,10 @@
                                         </div>
                                     </a>
                                     <!-- Message -->
-                                    <a href="javascript:void(0)" class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                        <span class="btn btn-info rounded-circle btn-circle"><i data-feather="settings" class="text-white"></i></span>
+                                    <a href="javascript:void(0)"
+                                        class="message-item d-flex align-items-center border-bottom px-3 py-2">
+                                        <span class="btn btn-info rounded-circle btn-circle"><i data-feather="settings"
+                                                class="text-white"></i></span>
                                         <div class="w-75 d-inline-block v-middle ps-2">
                                             <h6 class="message-title mb-0 mt-1">Settings</h6>
                                             <span class="font-12 text-nowrap d-block text-muted text-truncate">You
@@ -64,10 +88,13 @@
                                         </div>
                                     </a>
                                     <!-- Message -->
-                                    <a href="javascript:void(0)" class="message-item d-flex align-items-center border-bottom px-3 py-2">
-                                        <span class="btn btn-primary rounded-circle btn-circle"><i data-feather="box" class="text-white"></i></span>
+                                    <a href="javascript:void(0)"
+                                        class="message-item d-flex align-items-center border-bottom px-3 py-2">
+                                        <span class="btn btn-primary rounded-circle btn-circle"><i data-feather="box"
+                                                class="text-white"></i></span>
                                         <div class="w-75 d-inline-block v-middle ps-2">
-                                            <h6 class="message-title mb-0 mt-1">Pavan kumar</h6> <span class="font-12 text-nowrap d-block text-muted">Just
+                                            <h6 class="message-title mb-0 mt-1">Pavan kumar</h6> <span
+                                                class="font-12 text-nowrap d-block text-muted">Just
                                                 see the my admin!</span>
                                             <span class="font-12 text-nowrap d-block text-muted">9:02 AM</span>
                                         </div>
@@ -75,7 +102,8 @@
                                 </div>
                             </li>
                             <li>
-                                <a class="nav-link pt-3 text-center text-dark" href="javascript:void(0);">
+                                <a class="nav-link pt-3 text-center text-dark"
+                                    href="javascript:void(0);">
                                     <strong>Check all notifications</strong>
                                     <i class="fa fa-angle-right"></i>
                                 </a>
@@ -88,18 +116,30 @@
                 <!-- create new -->
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i data-feather="settings" class="svg-icon"></i>
+                    <a class="nav-link dropdown-toggle"
+                        href="#"
+                        id="navbarDropdown"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false">
+                        <i data-feather="settings"
+                            class="svg-icon"></i>
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-menu"
+                        aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item"
+                            href="#">Action</a>
+                        <a class="dropdown-item"
+                            href="#">Another action</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
+                        <a class="dropdown-item"
+                            href="#">Something else here</a>
                     </div>
                 </li>
                 <li class="nav-item d-none d-md-block">
-                    <a class="nav-link" href="javascript:void(0)">
+                    <a class="nav-link"
+                        href="javascript:void(0)">
                         <div class="customize-input">
                             <select class="custom-select form-control bg-white custom-radius custom-shadow border-0">
                                 <option selected>EN</option>
@@ -123,26 +163,45 @@
                 <!-- User profile and search -->
                 <!-- ============================================================== -->
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="javascript:void(0)" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="assets/images/users/profile-pic.jpg" alt="user" class="rounded-circle" width="40">
+                    <a class="nav-link dropdown-toggle"
+                        href="javascript:void(0)"
+                        data-bs-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false">
+                        <img src="{{ asset('assets/images/users/profile-pic.jpg') }}"
+                            alt="user"
+                            class="rounded-circle"
+                            width="40">
                         <span class="ms-2 d-none d-lg-inline-block"><span>Hello,</span> <span class="text-dark">Jason
-                                Doe</span> <i data-feather="chevron-down" class="svg-icon"></i></span>
+                                Doe</span> <i data-feather="chevron-down"
+                                class="svg-icon"></i></span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-end dropdown-menu-right user-dd animated flipInY">
-                        <a class="dropdown-item" href="javascript:void(0)"><i data-feather="user" class="svg-icon me-2 ms-1"></i>
+                        <a class="dropdown-item"
+                            href="javascript:void(0)"><i data-feather="user"
+                                class="svg-icon me-2 ms-1"></i>
                             My Profile</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i data-feather="credit-card" class="svg-icon me-2 ms-1"></i>
+                        <a class="dropdown-item"
+                            href="javascript:void(0)"><i data-feather="credit-card"
+                                class="svg-icon me-2 ms-1"></i>
                             My Balance</a>
-                        <a class="dropdown-item" href="javascript:void(0)"><i data-feather="mail" class="svg-icon me-2 ms-1"></i>
+                        <a class="dropdown-item"
+                            href="javascript:void(0)"><i data-feather="mail"
+                                class="svg-icon me-2 ms-1"></i>
                             Inbox</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="javascript:void(0)"><i data-feather="settings" class="svg-icon me-2 ms-1"></i>
+                        <a class="dropdown-item"
+                            href="javascript:void(0)"><i data-feather="settings"
+                                class="svg-icon me-2 ms-1"></i>
                             Account Setting</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="javascript:void(0)"><i data-feather="power" class="svg-icon me-2 ms-1"></i>
+                        <a class="dropdown-item"
+                            href="javascript:void(0)"><i data-feather="power"
+                                class="svg-icon me-2 ms-1"></i>
                             Logout</a>
                         <div class="dropdown-divider"></div>
-                        <div class="pl-4 p-3"><a href="javascript:void(0)" class="btn btn-sm btn-info">View
+                        <div class="pl-4 p-3"><a href="javascript:void(0)"
+                                class="btn btn-sm btn-info">View
                                 Profile</a></div>
                     </div>
                 </li>
