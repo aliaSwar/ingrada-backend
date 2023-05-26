d6adeb76b00398888ce566099d70a6d441240732
alia-sewar <130346497+alia-sewar@users.noreply.github.com>
2023-05-26 05:34:25 +0300

    Add End Point

--- routes/api.php
+++ routes/api.php
@@ -83,12 +83,27 @@ Route::name('app.')->prefix('app/')->whereNumber(['id'])->group(function () {
 
         //Order
         Route::name('order.')->prefix('order')->group(function () {
-            
+            Route::name('customize.')->prefix('customize')->group(function () {
+                //Types
                 Route::get('types',[CustomizeOrderController::class, 'getTypes']);
                 Route::post('types',[CustomizeOrderController::class, 'storeTypes']);
+                //values Type
                 Route::get('prefernces',[CustomizeOrderController::class, 'getPrefernces']);
                 Route::post('prefernces',[CustomizeOrderController::class, 'storePrefernces']);
+                //Information Order
                 Route::post('informations',[CustomizeOrderController::class, 'storeInformation']);
+                //Colors
+                Route::get('colors',[CustomizeOrderController::class, 'getColors']);
+                Route::get('suggest-colors',[CustomizeOrderController::class, 'getSuggestColors']);
+                Route::post('colors',[CustomizeOrderController::class, 'storeColors']);
+                //Fonts
+                Route::get('fonts',[CustomizeOrderController::class, 'getFonts']);
+                Route::get('suggest-fonts',[CustomizeOrderController::class, 'getSuggestFonts']);
+                Route::post('fonts',[CustomizeOrderController::class, 'storeFonts']);
+                //Designer
+                Route::get('designers',[CustomizeOrderController::class, 'getDesigners']);
+                Route::post('designers',[CustomizeOrderController::class, 'storeDesigner']);
+            });
         });
     });
 
