48645d011ab2e55a28c1c2c52d2e5c7409d065c2
alia-sewar <130346497+alia-sewar@users.noreply.github.com>
2023-05-26 05:34:06 +0300

    Update database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,10 @@ return new class extends Migration
             $table->string('fullname');
             $table->string('avatar')->nullable();
             $table->boolean('is_active')->default(true);
+            $table->boolean('is_deleted')->default(false);
             $table->integer('number_tasks')->unsigned()->nullable();
             $table->foreignId('category_id')->nullable(); //category users['creative','fast','extutable']
+            $table->string('category')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
