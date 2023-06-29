758f63a204fc53f52553b9c2c5654be92685bf5f
swar144 <swar2000alia@gmail.com>
2023-06-29 09:12:41 +0300

    Get Scopes

---
+++ app/Http/Controllers/Api/Configuration/ShowScopesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\Configuration;
+
+use App\Http\Controllers\Controller;
+use App\Models\Scope;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ShowScopesController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $scopes=Scope::query()->get();
+        return sendSuccessResponse(
+            __('messages.get_data'),
+            $scopes
+        );
+    }
+}
