8447c25e391529e373d42ac8a3079f805d22cb79
swar144 <swar2000alia@gmail.com>
2023-06-29 09:12:06 +0300

    get Prefernce

---
+++ app/Http/Controllers/Api/Configuration/ShowPreferncesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\Configuration;
+
+use App\Http\Controllers\Controller;
+use App\Models\Prefernce;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ShowPreferncesController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        $prefernce=Prefernce::query()->get();
+        return sendSuccessResponse(
+            __('messages.get_data'),
+            $prefernce
+        );
+    }
+}
