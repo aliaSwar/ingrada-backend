5ea9a25ac87fbedbe012cee104fb525d0321c86d
swar144 <swar2000alia@gmail.com>
2023-07-13 09:26:58 +0300

    update

---
+++ app/Http/Controllers/Api/Design/LikedDesignController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api\Design;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class LikedDesignController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function __invoke(Request $request): JsonResponse
+    {
+        return sendSuccessResponse(__('messages.get_data'));
+    }
+}
+
