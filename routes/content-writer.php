c10206f8ef7692104107910ca3b8ae41ebf15295
swar144 <swar2000alia@gmail.com>
2023-07-23 10:42:30 +0300

    sperate Routes

---
+++ routes/content-writer.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\Web\ContentWriter\ExternalOrderContentController;
+
+
+
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Content Writer Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register Content Writer routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "Content Writer" middleware group. Make something great!
+|
+*/
+
+
+     Route::middleware(['auth:sanctum'])->group(function (): void {
+          /**
+           * Protected endpoints.
+          */
+          Route::middleware(['content-writer'])->group(function (): void {
+               Route::resource('external-orders', ExternalOrderContentController::class);
+               
+          });
+     });
--- routes/web.php
+++ routes/web.php
@@ -35,47 +35,10 @@ use Illuminate\Support\Facades\Route;
 
 // Setting Route
 Route::middleware('auth:sanctum')->group(function (): void {
-Route::get('use', function () {
-    return auth()->user()->hasRole('admin');
-});
-    // Admin Route
-    Route::prefix('admin/')->group(function (): void {
-        Route::resource('roles', RoleController::class);
-        Route::resource('colors', ColorController::class);
-        Route::resource('fonts', FontController::class);
-        Route::resource('scopes', ScopeCotroller::class);
-        Route::resource('types', TypeCotroller::class);
-        Route::resource('users', RegisteredUserController::class);
-        Route::resource('categories', CategoryController::class);
-        Route::resource('prefernces', PrefernceController::class);
-        Route::get('prefernces-create/{type}', [PrefernceController::class, 'create'])->name('prefernc.create');
-        Route::post('prefernces-store/{type}', [PrefernceController::class, 'store'])->name('prefernc.store');
-        Route::resource('preferncesvalues', PrefernceValueController::class);
-        Route::get('preferncesvalues-create/{prefernce}', [PrefernceValueController::class, 'create'])->name('preferncesvalues.create');
-        Route::post('preferncesvalues-store/{prefernce}', [PrefernceValueController::class, 'store'])->name('preferncesvalues.store');
+    Route::get('use', function () {
+        return auth()->user()->hasRole('admin');
     });
 
-    // Manager Route
-    Route::prefix('manager/')->group(function (): void {
-        Route::resource('internal-customers', InternalCustomerController::class);
-        Route::resource('external-orders', ExternalOrderController::class);
-        Route::resource('internal-orders', InternalOrderController::class);
-    });
-    //designer Route
-    Route::prefix('designer/')->group(function (): void {
-        Route::resource('tasks', TaskController::class);
-        Route::get('tasks/create/{order}', [TaskController::class,'create'])->name('content-writer.tasks.create');
-        Route::post('tasks/{order}', [TaskController::class,'store'])->name('content-writer.tasks.store');
-        Route::get('/todo', [TaskController::class, 'get_todotask'])->name('to_do');
-        Route::get('/index_external', [TaskController::class, 'index_external'])->name('in_ex');
-        Route::get('/showexternal/{id}', [TaskController::class, 'show_external'])->name('to');
-        Route::post('/showexternal/{order}', [TaskController::class, 'store_external'])->name('showexternal');
-
-    });
-    Route::name('content-writer.')->prefix('content-writer/')->group(function (): void {
-        Route::resource('external-orders', ExternalOrderContentController::class);
-
-    });
 
     Route::get('/', function () {
         return view('dashboard');
